9ac9bc01d688f41382932c45269f5782a17976ea
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-03-04 16:21:46 +0530

    Time: 5 ms (50.05%) | Memory: 48.8 MB (5.03%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -9,17 +9,29 @@ class Solution {
                 list.add(ch);
             }
         }
-        int i=0;
-        int j = list.size()-1;
-        while(i<j){
-            if(!list.get(i).equals(list.get(j))){//in string check for .equals for checking equal characters instead of list.get(i) != list.get(j) -> this will check for objects different references in memory
+        // int j = list.size()-1;
+        // while(i<j){
+        //     if(!list.get(i).equals(list.get(j))){//in string check for .equals for checking equal characters instead of list.get(i) != list.get(j) -> this will check for objects different references in memory
+        //         return false;
+        //     }
+        //     else{
+        //         i++;
+        //         j--;
+        //     }
+        // }
+
+        
+        return palindrome(0,list);
+    }
+    
+    
+    boolean palindrome(int i , ArrayList<Character> list){
+         
+            if(i >= list.size()/2)return true;
+
+            if(!list.get(i).equals(list.get(list.size()-i-1))){
                 return false;
             }
-            else{
-                i++;
-                j--;
-            }
+            return palindrome(i+1,list);
         }
-        return true;
-    }
 }
