836ec5adf1fbcb6c33e2f08daab66563e7b1359a
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-03-08 10:22:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (58.45%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,17 +1,42 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int n= blocks.length();
+        // int n= blocks.length();
         
-        int mincount= Integer.MAX_VALUE;
-        for(int i=0;i<=n-k;i++){
-            int count = 0;
-            for(int j=i;j<i+k;j++){
-                if(blocks.charAt(j) == 'W'){
-                    count++;
-                }
-            }
-            mincount = Math.min(mincount ,count);
+        // int mincount= Integer.MAX_VALUE;
+        // for(int i=0;i<=n-k;i++){
+        //     int count = 0;
+        //     for(int j=i;j<i+k;j++){
+        //         if(blocks.charAt(j) == 'W'){
+        //             count++;
+        //         }
+        //     }
+        //     mincount = Math.min(mincount ,count);
+        // }
+        // return mincount;
+
+        
+    int n = blocks.length();
+    int count = 0, mincount;
+
+    // Count 'W' in the first window of size 'k'
+    for (int i = 0; i < k; i++) {
+        if (blocks.charAt(i) == 'W') {
+            count++;
         }
-        return mincount;
+    }
+
+    mincount = count; // Set initial minimum count
+
+    // Slide the window across the string
+    for (int i = k; i < n; i++) {
+        if (blocks.charAt(i - k) == 'W') count--; // Remove leftmost element
+        if (blocks.charAt(i) == 'W') count++; // Add new element
+        
+        mincount = Math.min(mincount, count);
+    }
+
+    return mincount;
+
+
     }
 }
