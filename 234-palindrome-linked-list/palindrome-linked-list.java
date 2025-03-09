b9897717212a64c81496dcb66dd00bd56988fb3c
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-03-10 00:51:01 +0530

    Time: 14 ms (15.22%) | Memory: 75.2 MB (5.09%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -8,25 +8,40 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public boolean isPalindrome(ListNode head) {
-        Stack<Integer> stack = new Stack<>();
-        ListNode temp = head;
-        while(temp != null){
-            stack.push(temp.val);
-            temp = temp.next;
-        }
-        temp = head;
-        while(temp!= null && !stack.isEmpty()){
-            if(stack.peek() == temp.val){
-                stack.pop();
-                temp = temp.next;
-            }
-            else{
-                return false;
-            }
-        }
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         Stack<Integer> stack = new Stack<>();
+//         ListNode temp = head;
+//         while(temp != null){
+//             stack.push(temp.val);
+//             temp = temp.next;
+//         }
+//         temp = head;
+//         while(temp!= null && !stack.isEmpty()){
+//             if(stack.peek() == temp.val){
+//                 stack.pop();
+//                 temp = temp.next;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
         
-        return true;
+//         return true;
+//     }
+// }
+
+class Solution {
+    ListNode curr;
+    public boolean isPalindrome(ListNode head) {
+        curr = head;
+        return solve(head);
+    }
+
+    public boolean solve(ListNode head) {
+        if(head == null) return true;
+        boolean ans = solve(head.next) && head.val == curr.val;
+        curr = curr.next;
+        return ans;
     }
 }
