21f1a4355f9046c498145d9851b73f8fdc5c8cc4
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-05-19 19:48:56 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (47.07%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -2,30 +2,30 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
         if(digits[n-1] != 9){
-            digits[n-1] = digits[n-1] + 1;
+            digits[n-1] = digits[n-1] + 1; // for 123 it will return 124
         }
         else{
             int m = n+1;
-            int arr[] = new int[m];
-            int i=n-1;
-            while(i>=0 && digits[i] == 9){
-                if(i==0 && digits[0] == 9){
-                    arr[0] = 1;
+            int newarr[] = new int[m];
+            int i = n-1;
+            while(i>=0 && digits[i] == 9){//jab tak 9 mil rha hai 
+                if(i==0 && digits[0] == 9){//for 999 wala numbers
+                    newarr[0] = 1;
                     for(int j=1;j<m;j++){
-                        arr[j] = 0;
+                        newarr[j] = 0;//inc and return 1000 
                     }
-                    return arr;
+                    return newarr;
                 }
                 else{
-                    digits[i] = 0;
+                    digits[i] = 0; // for 129 it will give 130 
                     i--;
-                }  
                 }
-                digits[i] = digits[i] +1;
                 
-
-            
+            }
+            int k = i;
+            digits[k] = digits[k] + 1;// 2 -> 3 and 9 will become 0 and give 130
         }
         return digits;
+
     }
 }
