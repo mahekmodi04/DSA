de2fb8e1fbea5ea4c8d1438ab6c5bd44c1e2e114
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-05-20 12:28:30 +0530

    Time: 6 ms (39.14%) | Memory: 53.4 MB (30.30%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,30 +1,30 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i] , map.getOrDefault(nums[i],0)+1);
-        }
-        int n = nums.length;
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-            if(entry.getValue() > n/2){
-                return entry.getKey();
-            }
-        }
-        return -1;
-        // Arrays.sort(nums);
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     map.put(nums[i] , map.getOrDefault(nums[i],0)+1);
+        // }
         // int n = nums.length;
-        // for(int i=1;i<n;i++){
-        //     int count =1;
-        //     if(nums[i] == nums[i-1]){
-        //         count++;
-        //     }
-        //     // else{
-        //     //     count =1;
-        //     // }
-        //     if(count > n/2){
-        //         return nums[i-1];
+        // for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+        //     if(entry.getValue() > n/2){
+        //         return entry.getKey();
         //     }
         // }
-        // return nums[0];
+        // return -1;
+        Arrays.sort(nums);
+        int count =1;
+        int n = nums.length;
+        for(int i=1;i<n;i++){
+            if(nums[i] == nums[i-1]){
+                count++;
+            }
+             else{
+                 count =1;
+            }
+            if(count > n/2){
+                return nums[i-1];
+            }
+        }
+        return nums[0];
     }
 }
