8caad1bd296c43088337ec77e285b0a6f4c9948d
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-05-21 19:59:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (29.81%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,10 +1,36 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == target){
-                return i;
+      //using binary search - modified
+      int n = nums.length;
+      int low = 0;
+      int high = n-1; 
+      while(low<=high){
+
+        int mid = low  +  (high - low)/2;
+
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[low] <= nums[mid]){
+            //left arr is sorted
+            if(target >= nums[low] && target < nums[mid]){
+                high = mid - 1;
+            }
+            else{//agar ye sahi nhi hai if condition then go to right array
+                low = mid + 1;
             }
         }
-        return -1;
+        else {
+            //right arr is sorted
+            if(target <= nums[high] && target > nums[mid]){
+                low = mid + 1;
+            }
+            else{//agar right array condition false hai toh go to left array 
+                high = mid - 1;
+            }
+        }
+        
+      } 
+      return -1;
     }
 }
