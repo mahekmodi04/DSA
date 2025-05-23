18fece949a4f40e49e6b2131bd20ae937862f434
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-05-23 17:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (96.46%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,26 +1,60 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
-        if(n==1){
-            return nums[0];
-        }
-        int i=1;
-        int count = 1;
-        while(i<nums.length){
-            if(nums[i] == nums[i-1]){
-                count++;
-                i++;
-            }else{
-                if(count > 1){
-                    i++;
-                    count = 1;
+
+         int n = nums.length;
+         int low=0;
+         int high= n-1;
+         while(low<high){
+            int mid = low+(high-low)/2;
+
+            if((mid == n-1 || nums[mid] != nums[mid+1]) && (mid == 0 || nums[mid] != nums[mid-1])){
+                return nums[mid];
+            }
+            else{
+                if(mid %2 == 0){
+                
+                    if(nums[mid] == nums[mid+1]){//on right side
+                        low = mid +1;
+                    }
+                    else{
+                        high = mid-1;
+                    }
                 }
                 else{
-                    return nums[i-1];
+                    
+                    if(nums[mid] == nums[mid+1]){//on left side
+                        high = mid-1;
+                    }
+                    else{
+                        low = mid +1;
+                    }
+
                 }
-                
             }
-        }
+            
+            
+         }
+         return nums[low];
+        // if(n==1){
+        //     return nums[0];
+        // }
+        // int i=1;
+        // int count = 1;
+        // while(i<nums.length){
+        //     if(nums[i] == nums[i-1]){
+        //         count++;
+        //         i++;
+        //     }else{
+        //         if(count > 1){
+        //             i++;
+        //             count = 1;
+        //         }
+        //         else{
+        //             return nums[i-1];
+        //         }
+                
+        //     }
+        // }
         
         // int prev = -1;
         // int after = 1;
@@ -36,7 +70,7 @@ class Solution {
         //         return nums[i];
             
         // }
-         return nums[n-1];
+        // return nums[n-1];
 
     }
 }
