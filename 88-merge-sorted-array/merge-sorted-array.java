148ef71ffd0e8ca0bf783dee323fe84e8c815d5e
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-05-24 02:58:57 +0530

    Time: 15 ms (2.67%) | Memory: 43 MB (8.28%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,19 +1,51 @@
 class Solution {
     public void merge(int[] a, int m, int[] b, int n) {
-        if(a.length-m != n){
+        
+        // int i=0;
+        // int j=0;
+        // while(i<a.length && j<n){
+        //     if(a[i] < b[j] && a[i] !=0){
+        //         i++;
+        //     }
+        //     else if(a[i] < b[j] && a[i] == 0){
+        //         a[i] = b[j];
+        //     }
+        //     else if(a[i] == b[j]){
+        //         i++;
+        //         int temp = a[i];
+        //         a[i] = b[j];
+        //     }
+        //     else{
+                
+        //         j++;
+            
+            
+        // }
+        // Arrays.sort(a);
+        // for(i=0;i<a.length;i++){
+        //     System.out.print(a[i]);
+        // }
+
+        if (a.length - m != n) {
             System.out.print("-1");
+            return; // Stop execution
         }
-        int i=m;
-        int j=0;
-        while(i<a.length && j<n){
+
+        int i = m; // Start from the last index of array a
+        int j = 0;            // Start from beginning of array b
+
+        while (j < n) {
             a[i] = b[j];
-            i++;
             j++;
+            i++;
         }
+
         Arrays.sort(a);
-        for(i=0;i<a.length;i++){
-            System.out.print(a[i]);
+
+        for (i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
         }
+
             
     }
 }
