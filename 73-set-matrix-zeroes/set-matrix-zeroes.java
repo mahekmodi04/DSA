dffb92743ecc0f83132454b8c9e9392cd61e3f5e
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-05-26 09:19:01 +0530

    Time: 1 ms (72.49%) | Memory: 45.8 MB (44.43%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == 0){
+                    makerow(i,matrix);
+                    makecol(j,matrix);
+                }
+            }
+        } 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == 'c'){
+                    matrix[i][j] = 0;
+                }
+            }
+        } 
+    }
+
+    public void makerow(int i,int matrix[][]){
+        for(int j=0;j<matrix[0].length;j++){
+            if(matrix[i][j] != 0){
+                matrix[i][j] = 'c';
+            }
+        }
+    }
+
+    public void makecol(int j,int matrix[][]){
+        for(int i=0;i<matrix.length;i++){
+            if(matrix[i][j] != 0){
+                matrix[i][j] = 'c';
+            }
+        }
+    }
+}
