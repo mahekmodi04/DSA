432fc972b0d777a63f417fc6080f3cf91dc6d738
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-06-02 00:45:53 +0530

    Time: 3 ms (99.65%) | Memory: 68.6 MB (37.79%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -8,41 +8,42 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public boolean isPalindrome(ListNode head) {
-//         Stack<Integer> stack = new Stack<>();
-//         ListNode temp = head;
-//         while(temp != null){
-//             stack.push(temp.val);
-//             temp = temp.next;
-//         }
-//         temp = head;
-//         while(temp!= null && !stack.isEmpty()){
-//             if(stack.peek() == temp.val){
-//                 stack.pop();
-//                 temp = temp.next;
-//             }
-//             else{
-//                 return false;
-//             }
-//         }
-        
-//         return true;
-//     }
-// }
 
+ //---------U CAN OPTIMIZE THIS CODE-----------//
+ 
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> stack = new Stack();
-        ListNode curr = head;
-        while(curr != null) {
-            stack.push(curr.val);
-            curr = curr.next;
+        //step1: to find middle node
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        curr = head;
-        while(curr != null && curr.val == stack.pop()) {
-            curr = curr.next;
+
+        // step2 - to reverse the second half of the LL
+        //ListNode mid = slow;
+        ListNode prev = null;
+        ListNode curr = slow;
+
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
-        return curr == null;
+
+        //step3 - to check the left and right part to check if palindrome
+        ListNode left = head;
+        ListNode right = prev;
+        while(right != null){
+            if(left.val != right.val){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+
     }
 }
