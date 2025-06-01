2543709c48c03dd611a17799209857a3f20a0c51
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-06-01 21:10:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (19.45%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -27,15 +27,29 @@ class Solution {
         // return head;
 
         //method II
-        ListNode temp = head;
-        ListNode prev = null;
-        while(temp != null){
-            ListNode front = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = front;
+        // ListNode temp = head;
+        // ListNode prev = null;//instead of using extra space...we can use this 1-2-3-4-5
+        // //we want to make it 5-4-3-2-1
+        // //so we will take prev , front and temp as 3 nodes in consdieration..till we get null we will run this loop and we will assign front as our temp.next
+        // while(temp != null){
+        //     ListNode front = temp.next;
+        //     temp.next = prev;//prev is declared null for the first node later it will incr to temp and 
+        //     prev = temp;//we are moving -> to <- basically so temp.next instead of front it will point prev node 
+        //     temp = front;
+        //     //temp will incr to front 
 
+        // }
+        // return prev;// at the end head will become 1 and prev will be 5 so to get the whole ll we will return prev;
+
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
-        return prev;
+        head = prev;
+        return head;
     }
 }
