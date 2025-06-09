f5f3b3fae6331e12487ceb7606ec058f945ad2f3
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-06-09 13:41:15 +0530

    Time: 1 ms (99.54%) | Memory: 45.7 MB (65.30%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int arr[] = new int[2];
+        long XOR = 0;
+        for(int i=0;i<nums.length;i++){
+            XOR ^= nums[i];
+        }
+        long rightmost = ((XOR & (XOR - 1)) ^ XOR);
+        int b1 = 0;
+        int b2 = 0;
+        for(int j=0;j<nums.length;j++){
+            if((rightmost & nums[j]) != 0){
+                b1 ^= nums[j];
+            }
+            else{
+                b2 ^= nums[j];
+            }  
+        }
+        arr[0] = b1;
+        arr[1] = b2;
+        return arr;
+    }
+}
