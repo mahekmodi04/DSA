68b745df6f5cf1cb1a5aa62d7b0d6afe4107525a
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-06-12 12:23:28 +0530

    Time: 4 ms (15.64%) | Memory: 43.9 MB (12.06%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+       Set<List<Integer>> set = new HashSet<>();
+       Arrays.sort(nums);
+       int n = nums.length;
+       int subsets  = 1<<n;
+       for(int i=0;i<subsets;i++){
+        List<Integer> list = new ArrayList<>();
+        for(int j=0;j<nums.length;j++){
+            //check the ith bit is set or no;
+            if((i & (1<<j)) != 0){
+                list.add(nums[j]);
+            }
+        }
+        Collections.sort(list); 
+        set.add(list);
+       }
+       List<List<Integer>> mlist = new ArrayList<>(set);
+       return mlist;
+    }
+}
