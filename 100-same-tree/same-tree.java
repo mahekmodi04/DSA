7257612fd48eaeffb3a4590721876e89f5fd4804
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-06-29 15:02:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (40.81%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,69 +15,36 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p== null && q==null){
-            return true;
-        }
-        if(p==null || q == null ){
-            return false;
-        }
         Queue<TreeNode> q1 = new LinkedList<>();
         Queue<TreeNode> q2 = new LinkedList<>();
+
+        if(p==null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val != q.val){
+            return false;
+        }
         q1.add(p);
         q2.add(q);
         while(!q1.isEmpty() && !q2.isEmpty()){
-           
-                    TreeNode node1 = q1.poll();
-                    TreeNode node2 = q2.poll();
-                    if(node1 == null && node2 == null){ 
-                        continue;
-                    }
-                    
-                    if(node1 == null || node2 == null || node1.val != node2.val){
-                        return false;
-                    }
-                    q1.add(node1.left);
-                    q1.add(node1.right);
-                    q2.add(node2.left);
-                    q2.add(node2.right);
-                    
-                   
+            TreeNode n1 = q1.poll();
+            TreeNode n2 = q2.poll();
+            if(n1 == null && n2 == null){
+                continue;
             }
-            return q1.isEmpty() && q2.isEmpty();
+            if(n1 == null || n2 == null || n1.val != n2.val){
+                return false;
+            }
+            q1.add(n1.left);
+            q1.add(n1.right);
+            q2.add(n2.left);
+            q2.add(n2.right);
         }
+        return q1.isEmpty() && q2.isEmpty();
         
     }
-
-    // public boolean isSameTree(TreeNode p, TreeNode q) {
-    //     if (p == null && q == null) {
-    //         return true;
-    //     }
-    //     if (p == null || q == null) {
-    //         return false;
-    //     }
-
-    //     Queue<TreeNode> q1 = new LinkedList<>();
-    //     Queue<TreeNode> q2 = new LinkedList<>();
-    //     q1.add(p);
-    //     q2.add(q);
-
-    //     while (!q1.isEmpty() && !q2.isEmpty()) {
-    //         TreeNode node1 = q1.poll();
-    //         TreeNode node2 = q2.poll();
-
-    //         if (node1 == null && node2 == null) {
-    //             continue;
-    //         }
-    //         if (node1 == null || node2 == null || node1.val != node2.val) {
-    //             return false;
-    //         }
-
-    //         // Add both left and right children to the queue, even if they are null
-    //         q1.add(node1.left);
-    //         q1.add(node1.right);
-    //         q2.add(node2.left);
-    //         q2.add(node2.right);
-    //     }
-
-    //     return q1.isEmpty() && q2.isEmpty();
-    // }
+    
+}
