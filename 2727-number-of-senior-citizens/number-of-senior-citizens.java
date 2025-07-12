fab4b462128a4b5aa29826772f631a083229611a
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-07-13 02:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (73.54%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -3,10 +3,10 @@ class Solution {
         int n = 0;
         for(int i=0;i<details.length;i++){
             String s = details[i];
-            if(s.charAt(s.length() - 4) == '6' && s.charAt(s.length() - 3) == '0'){
+            if(s.charAt(11) == '6' && s.charAt(12) == '0'){
                 continue;
             }
-            if(s.charAt(s.length() - 4) >= '6' && s.charAt(s.length() - 3) >= '0'){
+            if(s.charAt(11) >= '6' && s.charAt(12) >= '0'){
                 n++;
             }
         }
