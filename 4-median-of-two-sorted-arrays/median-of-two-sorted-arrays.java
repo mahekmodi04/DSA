9c0c43d50ad7de752ae1a6b61667f54868df745e
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-07-12 18:09:38 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (44.58%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,30 +2,56 @@ class Solution {
     public double findMedianSortedArrays(int[] a, int[] b) {
         int n = a.length;
         int m = b.length;
-        int[] ans = new int[n + m];
-
-        int i = 0, j = 0, k = 0;
-        while (i < n && j < m) {
-            if (a[i] < b[j]) {
-                ans[k++] = a[i++];
-            } else {
-                ans[k++] = b[j++];
+        int ans[] = new int[m+n];
+        int i=0;
+        int j=0;
+        int k =0;
+        while(i<a.length && j<b.length){
+            if(a[i] < b[j]){
+                ans[k] = a[i];
+                // while(i < a.length-1 && a[i] == a[i+1]){
+                //     i++;
+                // }
+                i++;
+                k++;
             }
+            else{
+                ans[k] = b[j];
+                // while( j < b.length-1 && b[j] == b[j+1]){
+                //     j++;
+                // }
+                j++;
+                k++;
+            }
+            // else{
+            //     ans[k] = a[i];
+            //     // while(i < a.length-1 && a[i] == a[i+1]){
+            //     //     i++;
+            //     // }
+            //     i++;
+            //     // while(j < b.length-1 && b[j] == b[j+1]){
+            //     //     j++;
+            //     // }
+            //     j++;
+            //     k++;
+            // }
+        }
+        while(i<a.length){
+            ans[k] = a[i];
+            i++;
+            k++;
+        }
+        while(j<b.length){
+            ans[k] = b[j];
+            j++;
+            k++;
         }
 
-        while (i < n) {
-            ans[k++] = a[i++];
+        if(ans.length % 2 == 0){
+            int len = ans.length/2;
+            return (ans[len] + ans[len-1])/ 2.0;
         }
-
-        while (j < m) {
-            ans[k++] = b[j++];
-        }
-
         int len = ans.length;
-        if (len % 2 == 0) {
-            return (ans[len / 2] + ans[(len / 2) - 1]) / 2.0;
-        } else {
-            return ans[len / 2];
-        }
+        return ans[len/2];
     }
 }
