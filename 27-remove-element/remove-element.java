db94df678e004b4d4ec5915cc970642c389402a5
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-07-13 02:13:16 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (67.38%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,14 +1,23 @@
-class Solution {//try this que again 
+class Solution {
     public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int j = 0;
         int n = nums.length;
-        int j=0;
-        for(int i=0;i<n;i++){
-            if(nums[i] != val){
-                nums[j] = nums[i];
+
+        while (j < n) {
+            if (nums[j] == val) {
+                i = j;
+                while (i < n - 1) {
+                    nums[i] = nums[i + 1]; // shift left
+                    i++;
+                }
+                n--; // reduce effective size
+                // don't increment j, need to check new nums[j] again
+            } else {
                 j++;
             }
         }
-        return j;
+
+        return n; // new length
     }
-    
 }
