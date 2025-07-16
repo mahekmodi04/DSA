097f0dc04100301a33dda84bbb8f4a3d9c7f878c
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-07-16 14:43:21 +0530

    Time: 8 ms (10.03%) | Memory: 45.5 MB (79.97%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,44 +1,30 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        // int arr[] = new int[2];
-        // HashMap<Integer,Integer> hash = new HashMap<>();
-        // for(int i=0;i<grid.length;i++){
-        //     for(int j=0;j<grid[0].length;j++){
-        //         if(!hash.containsKey(grid[i][j])){
-        //             hash.put(grid[i][j] , 1);
-        //         }
-        //         else{
-                   
-        //             arr[0] = grid[i][j];
-        //             hash.put(grid[i][j] , hash.get(grid[i][j])+1);
-                    
-        //         }
-        //     }
-        // }
-        // int n = grid.length;
-        // for(int i=1;i<=n*n;i++){
-        //     if(!hash.containsKey(i)){
-        //         arr[1] = i; 
-        //     }
-        // }
-        // return arr;
-
+        int ans[] = new int[2];
+        int k = 1;
         int n = grid.length;
-        int freq[] = new int[n*n+1];
-        for(int i=0;i<grid.length;i++){
-             for(int j=0;j<grid[0].length;j++){
-                int num = grid[i][j];
-                freq[num]++;
-             }
-        }
-        int arr[] = new int[2];
-        for(int i=0;i<freq.length;i++){
-            if(freq[i] == 2){
-                arr[0] =i;
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(!set.add(grid[i][j])){
+                    ans[0] = grid[i][j];
+                }
             }
-            if(freq[i] == 0){
-                arr[1] = i;            }
         }
-        return arr;
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        int i =0;
+        while(k <= n*n){
+            if(i< list.size() && k == list.get(i)){
+                i++;
+                k++;
+            }
+            else{
+                ans[1] = k;
+                break;
+            }
+        }
+        return ans;
     }
 }
