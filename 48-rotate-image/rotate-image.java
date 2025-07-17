1753e036d544219314922ffe6d11032a28e7ba12
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-07-17 16:36:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (25.49%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,61 +1,26 @@
 class Solution {
-    public void rotate(int[][] nums) {
-        // int n = matrix.length;
-        // //transpose
-        // for(int i=0;i<n;i++){
-        //     for(int j=i+1;j<n;j++){
-        //         int temp = matrix[i][j];
-        //         matrix[i][j] = matrix[j][i];
-        //         matrix[j][i] = temp;
-        //     }
-            
-        // }
-        // //swap the rows
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n/2;j++){
-        //         int temp = matrix[i][j];
-        //         matrix[i][j] = matrix[i][n-j-1];
-        //         matrix[i][n-j-1] = temp;
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-            
-        //         System.out.print(matrix[i][j]);
-        //     }
-            
-        // }
-
-        int n = nums.length;
-        //transpose
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int temp = nums[i][j];
-                nums[i][j] = nums[j][i];
-                nums[j][i] = temp;
+    public void rotate(int[][] arr) {
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr[0].length;j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
             }
         }
-
-        //rotate 
-        int i=0;
-        int j=n-1;
-        while(i<=j){
-            for(int k=0;k<n;k++){
-                int temp = nums[k][i];
-                nums[k][i] = nums[k][j];
-                nums[k][j] = temp;
+        //col swap
+        int i = 0;
+        int j = arr[0].length - 1;
+        
+        while(i<j){
+            int k = 0;
+            while(k < arr.length ){
+            int temp = arr[k][i];
+            arr[k][i] = arr[k][j];
+            arr[k][j] = temp;
+            k++;
             }
             i++;
             j--;
         }
-
-        //print
-        for(i=0;i<n;i++){
-            for(j=0;j<n;j++){
-                System.out.print(nums[i][j]);
-            }
-        }
-
-        
     }
 }
