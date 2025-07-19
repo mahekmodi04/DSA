8e2279f4acd722651dfb8178d5016a735f885a70
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-07-19 16:32:15 +0530

    Time: 2 ms (80.40%) | Memory: 45.6 MB (98.66%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,17 +1,37 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         //brute force - to use temp take all non zeros and then push all zeros back 
-        ArrayList<Integer> list = new ArrayList<>();
+        // ArrayList<Integer> list = new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i] != 0){
+        //         list.add(nums[i]);
+        //     }
+        // }
+        // for(int i=0;i<list.size();i++){
+        //     nums[i] = list.get(i);
+        // }
+        // for(int i=list.size();i<nums.length;i++){
+        //     nums[i] = 0;
+        // }
+
+        int j = -1;
         for(int i=0;i<nums.length;i++){
-            if(nums[i] != 0){
-                list.add(nums[i]);
+            if(nums[i] == 0){
+                j=i;
+                break;
             }
         }
-        for(int i=0;i<list.size();i++){
-            nums[i] = list.get(i);
-        }
-        for(int i=list.size();i<nums.length;i++){
-            nums[i] = 0;
+        if(j==-1)return;
+
+        int i = j+1;
+        while( i<nums.length){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+            i++;
         }
     }
 }
