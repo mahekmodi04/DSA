31479ddab55daf30929858fce930f536c7d5a523
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-07-20 11:30:47 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (74.16%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,43 +1,42 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int bp = 0;
-        int ref = 0;
-        for(int i=nums.length-1 ; i>0;i--){
+        int bp = -1;
+        int n = nums.length;
+        for(int i=n-1;i>0;i--){
             if(nums[i] > nums[i-1]){
-                bp = i;
-                ref = i-1;
+                bp = i-1;
                 break;
             }
         }
-        if(bp == 0){
+        if(bp == -1){
             int first = 0;
-            int last = nums.length-1;
+            int last = n-1;
             while(first <= last){
-            int t = nums[first];
-            nums[first] = nums[last];
-            nums[last] = t;
-            first++;
-            last--;
-        }
-        return;
-        }
-        for(int j=nums.length-1;j>=bp;j--){
-            if(nums[j] > nums[ref]){
-                int temp = nums[ref];
-                nums[ref] = nums[j];
-                nums[j] = temp;
-                break;
+                int t = nums[first];
+                nums[first] = nums[last];
+                nums[last] = t;
+                first++;
+                last--;
             }
+            return;
         }
-        //reverse 
-        int first = bp;
-        int last = nums.length-1;
-        while(first <= last){
-            int t = nums[first];
-            nums[first] = nums[last];
-            nums[last] = t;
-            first++;
-            last--;
+        //int smallest = 0;
+        for(int i=n-1;i>bp;i--){
+            if(nums[i] > nums[bp]){
+                int t = nums[bp];
+                nums[bp] = nums[i];
+                nums[i] = t;
+                 break;
+            }
+           
+        }
+        int left = bp + 1, right = n - 1;
+        while(left < right) {
+            int t = nums[left];
+            nums[left] = nums[right];
+            nums[right] = t;
+            left++;
+            right--;
         }
     }
 }
