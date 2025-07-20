cfc3705c38ca3ffe00250fcc210051cc90a68443
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-07-20 10:08:29 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (65.63%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,32 +1,25 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        // int n = nums.length;
-        // for(int i=0;i<n-1;i++){
-        //     for(int j=0;j<n-1-i;j++){
-        //         if(nums[j] > nums[j+1]){
-        //             int temp = nums[j];
-        //             nums[j] = nums[j+1];
-        //             nums[j+1] = temp;
-        //         }
-        //     }
-        // }
-    
-
-        int n = nums.length;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i] > nums[j]){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
+    public void sortColors(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length-1;
+        while(mid <= high){
+            if(arr[mid] == 0){
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+                mid++;
+                low++;
+            }
+            else if(arr[mid] == 2){
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+            else{
+                mid++;
             }
         }
-
-        for(int i=0;i<n;i++){
-            System.out.print(nums[i] + ",");
-        }
-
-
     }
 }
