87fbfda669dd67ba9a91662d56dad601fb13fac9
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-07-27 13:56:34 +0530

    Time: 1 ms (90.46%) | Memory: 41.7 MB (38.41%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,42 +1,25 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        // int n= blocks.length();
-        
-        // int mincount= Integer.MAX_VALUE;
-        // for(int i=0;i<=n-k;i++){
-        //     int count = 0;
-        //     for(int j=i;j<i+k;j++){
-        //         if(blocks.charAt(j) == 'W'){
-        //             count++;
-        //         }
-        //     }
-        //     mincount = Math.min(mincount ,count);
-        // }
-        // return mincount;
-
-        
-    int n = blocks.length();
-    int count = 0, mincount;
-
-    // Count 'W' in the first window of size 'k'
-    for (int i = 0; i < k; i++) {
-        if (blocks.charAt(i) == 'W') {
-            count++;
+        int swaps = 0;
+        int w = 0;
+        int min = 0;
+        //count white in first window
+        for(int i=0;i<k;i++){
+            if(blocks.charAt(i) == 'W'){
+                w++;
+            }
         }
-    }
-
-    mincount = count; // Set initial minimum count
-
-    // Slide the window across the string
-    for (int i = k; i < n; i++) {
-        if (blocks.charAt(i - k) == 'W') count--; // Remove leftmost element
-        if (blocks.charAt(i) == 'W') count++; // Add new element
-        
-        mincount = Math.min(mincount, count);
-    }
-
-    return mincount;
-
-
+        swaps = w;
+        min = swaps;
+        for(int i=0,j=k;j<blocks.length();i++,j++){
+            if(blocks.charAt(i) == 'W'){
+                swaps--;
+            }
+            if(blocks.charAt(j) == 'W'){
+                swaps++;
+            }
+            min = Math.min(min , swaps);
+        }
+        return min;
     }
 }
