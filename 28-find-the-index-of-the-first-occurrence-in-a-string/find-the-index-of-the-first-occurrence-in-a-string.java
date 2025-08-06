74a4dd813d5f857e04e0dde5c248fcab531d4fdf
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-08-06 11:47:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (89.83%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,24 +1,12 @@
-//more efficient code 
-
 class Solution {
-    public int strStr(String haystack, String needle) {
-
-        int n = haystack.length();
-        int m = needle.length();
-        int k = 0;
-        for(int i=0;i<n;i++){
-            if(haystack.charAt(i)==needle.charAt(0)){//we dont check for each substring of haystack instead we check if the starting value of needle matches with which index of haystack 
-                k = i;//assign that index to k 
-                if(k+m <= n){//check for substring if k+m is still a partfo haystack string if not return -1
-                String substr = haystack.substring(k,k+m); // create a substring from that k index where the character matches to the length of needle which is k+m 
-                    if(substr.equals(needle)){ // if both strings are exactly same then it will return k which is the initial index 
-                        return k;
-                    }
-                    //else it will increment and check for next character if matches needle(0) and repeat 
-                }
-                
-            }
+    public int strStr(String s1, String s2) {
+        int j = s2.length();
+       for(int i=0;i<s1.length()-j+1;i++){
+        if(s1.charAt(i) == s2.charAt(0)){
+            String sub = s1.substring(i,i+j);
+            if(sub.equals(s2))return i;
         }
-        return -1;//if nothing matches with needle(0) then return -1
+       }
+       return -1;
     }
 }
