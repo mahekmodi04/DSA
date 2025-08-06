9fc0edf24bae72567b7f10d3c9d4d90caf76168e
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-08-06 11:09:08 +0530

    Time: 1 ms (21.82%) | Memory: 41.5 MB (67.30%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length() != goal.length()){
-            return false;
-        }
-        String str = s+s;
-        if(str.contains(goal)){
-            return true;
+        if(s.length() != goal.length())return false;
+        String str = s + s;
+        int i=0;
+        int j=goal.length();
+        while(j<str.length()){
+            String substr = str.substring(i,j);
+            if(goal.equals(substr)){
+                return true;
+            }
+            i++;
+            j++;
         }
         return false;
     }
