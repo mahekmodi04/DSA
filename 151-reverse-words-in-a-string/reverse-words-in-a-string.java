8e4b37081a3c3f3224f65cf25677518fc7e70507
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-08-08 14:30:23 +0530

    Time: 3 ms (98.92%) | Memory: 42.5 MB (98.24%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,43 +1,23 @@
 class Solution {
     public String reverseWords(String s) {
-        // StringBuilder sb= new StringBuilder("");
-         int n = s.length();
-        // int i = n-1;
-        // while(i>=0){
-        //     while(i>=0 && s.charAt(i)==' '){
-        //     i--;
-        //     }
-        //     int j = i;
-        //     while(i>=0 && s.charAt(i) != ' '){
-        //         i--;
-        //     }
-        //     if(sb.length() > 0){
-        //         sb.append(" ");
-        //     }
-        //     sb.append(s.substring(i+1,j+1));
-        // }
-        // return sb.toString();
-
         StringBuilder str = new StringBuilder("");
-        int i = n-1; 
-        while(i >= 0 && s.charAt(i)== ' '){
+        int i = s.length()-1;
+        while(i>= 0 && s.charAt(i) == ' '){
             i--;
         }
-        
-        while(i >=0){
-            int k = i;
-            while(i >= 0 && s.charAt(i) != ' '){
-                i--;
+        while(i>= 0){
+            int j = i;
+            while(j>= 0 && s.charAt(j)!= ' '){
+                j--;
             }
-            str.append(s.substring(i+1 , k+1));
-            str.append(" ");
-            while(i>=0 && s.charAt(i) == ' '){
-                i--;
+            str.append(s.substring(j+1,i+1));
+            str.append(' ');
+            while(j>= 0 && s.charAt(j) == ' '){
+                j--;
             }
-            
+            i = j;
         }
-        
-        return str.toString().trim();
-        }
-        
+        str.deleteCharAt(str.length()-1);
+        return str.toString();
+    }
 }
