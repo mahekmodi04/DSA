954f3dadc40bcae21b01b0bf062e62e4538dddbf
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-08-13 10:59:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (56.89%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,42 +1,43 @@
 class Solution {
     public void nextPermutation(int[] nums) {
+        //1.find the breaking pt
         int bp = -1;
-        int n = nums.length;
-        for(int i=n-1;i>0;i--){
+        for(int i=nums.length-1;i>0;i--){
             if(nums[i] > nums[i-1]){
-                bp = i-1;
+                bp = i;
                 break;
             }
         }
+        
         if(bp == -1){
-            int first = 0;
-            int last = n-1;
-            while(first <= last){
-                int t = nums[first];
-                nums[first] = nums[last];
-                nums[last] = t;
-                first++;
-                last--;
+            int i=0;
+            int j = nums.length-1;
+            while(i<j){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
             }
             return;
         }
-        //int smallest = 0;
-        for(int i=n-1;i>bp;i--){
-            if(nums[i] > nums[bp]){
-                int t = nums[bp];
-                nums[bp] = nums[i];
-                nums[i] = t;
-                 break;
+        int tar = bp-1;
+        for(int i=nums.length-1;i>=bp ;i--){
+            if(nums[i] > nums[tar]){
+                int temp = nums[i];
+                nums[i] = nums[tar];
+                nums[tar] = temp;
+                break;
             }
-           
         }
-        int left = bp + 1, right = n - 1;
-        while(left < right) {
-            int t = nums[left];
-            nums[left] = nums[right];
-            nums[right] = t;
-            left++;
-            right--;
+        int i=bp;
+        int j = nums.length-1;
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
         }
     }
 }
