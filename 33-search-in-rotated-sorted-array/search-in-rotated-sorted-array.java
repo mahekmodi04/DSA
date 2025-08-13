eb64205a46d1984d8c4cacca472b0c3025656b9b
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-08-13 10:30:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (68.35%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,36 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
-      //using binary search - modified
-      int n = nums.length;
-      int low = 0;
-      int high = n-1; 
-      while(low<=high){
-
-        int mid = low  +  (high - low)/2;
-
-        if(nums[mid] == target){
-            return mid;
-        }
-        else if(nums[low] <= nums[mid]){
-            //left arr is sorted
-            if(target >= nums[low] && target < nums[mid]){
-                high = mid - 1;
+        int low = 0;
+        int high = nums.length-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(target == nums[mid]){
+                return mid;
             }
-            else{//agar ye sahi nhi hai if condition then go to right array
-                low = mid + 1;
+            //left sorted
+            else if(nums[mid] >= nums[low]){
+                if(target >= nums[low] && target<= nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid+1;
+                }
             }
-        }
-        else {
-            //right arr is sorted
-            if(target <= nums[high] && target > nums[mid]){
-                low = mid + 1;
-            }
-            else{//agar right array condition false hai toh go to left array 
-                high = mid - 1;
+            //right sorted
+            else{
+                if(target >= nums[mid] && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                } 
             }
         }
-        
-      } 
-      return -1;
+        return -1;
     }
 }
