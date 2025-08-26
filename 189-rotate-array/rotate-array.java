e7184df15515e39b9aab37a2454c7b0d6644454d
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-08-26 08:17:17 +0530

    Time: 2 ms (25.32%) | Memory: 57.5 MB (53.22%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,38 +1,33 @@
 class Solution {
-    public void rotate(int[] arr, int k) {
-        int n = arr.length;
-        int first =0;
-        int last = n-1;
-        k = k % n;
-        while(first <= last){
-            int temp = arr[first];
-            arr[first] = arr[last];
-            arr[last] = temp;
-            first++;
-            last--;
+    public void rotate(int[] nums, int k) {
+        int i=0;
+        int j = nums.length-1;
+        k = k % nums.length;
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
         }
-        first = 0;
-        last = k-1;
-        while(first <= last){
-            int temp = arr[first];
-            arr[first] = arr[last];
-            arr[last] = temp;
-            first++;
-            last--;
+        i = 0;
+        j= k-1;
+        while(i<=j){
+           int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--; 
         }
-        first = k;
-        last = n-1;
-        while(first <= last){
-            int temp = arr[first];
-            arr[first] = arr[last];
-            arr[last] = temp;
-            first++;
-            last--;
+        i = k;
+        j = nums.length-1;
+         while(i<=j){
+           int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--; 
         }
-
-        //print
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]);
-        }
+        
     }
 }
