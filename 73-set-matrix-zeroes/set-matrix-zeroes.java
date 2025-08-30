84e2e889b37c089bb3ac605786324fc5e091e616
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-08-30 09:45:58 +0530

    Time: 1 ms (71.70%) | Memory: 45.8 MB (46.77%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,31 +1,37 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j] == 0){
-                    makerow(i,matrix);
-                    makecol(j,matrix);
+    public void setZeroes(int[][] mat) {
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j] == 0){
+                    makerow(i,mat);
+                    makecol(j,mat);
                 }
             }
         }
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j] == 'c'){
-                    matrix[i][j] = 0;
+        
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j] == 'c'){
+                    mat[i][j] = 0;
                 }
             }
         }
         
     }
-    public static void makerow(int i , int[][] matrix){
-        for(int c=0;c<matrix[0].length;c++){
-            if(matrix[i][c] != 0)matrix[i][c] = 'c';
+    public static void makerow(int i, int mat[][]){
+            
+        for(int j=0;j<mat[0].length;j++){
+            if(mat[i][j] != 0){
+                mat[i][j] = 'c';
+            }  
         }
     }
-    public static void makecol(int j , int[][] matrix){
-        for(int r=0;r<matrix.length;r++){
-            if(matrix[r][j] != 0)matrix[r][j] = 'c';
+    public static void makecol(int j, int mat[][]){
+            
+        for(int i=0;i<mat.length;i++){
+            if(mat[i][j] != 0){
+                mat[i][j] = 'c';
+            }  
         }
     }
 }
