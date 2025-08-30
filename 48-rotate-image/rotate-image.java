a5dc114262c474c12368939dec0baa01443c2003
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2025-08-30 09:58:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (7.86%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,23 +1,20 @@
 class Solution {
     public void rotate(int[][] arr) {
-        for(int i=0;i<arr.length-1;i++){
+        //transpose
+        for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr[0].length;j++){
                 int temp = arr[i][j];
                 arr[i][j] = arr[j][i];
                 arr[j][i] = temp;
             }
         }
-        //col swap
-        int i = 0;
-        int j = arr[0].length - 1;
-        
-        while(i<j){
-            int k = 0;
-            while(k < arr.length ){
-            int temp = arr[k][i];
-            arr[k][i] = arr[k][j];
-            arr[k][j] = temp;
-            k++;
+        int i=0;
+        int j=arr[0].length-1;
+        while(i < j){
+            for(int k=0;k<arr.length;k++){
+                int temp = arr[k][i];
+                arr[k][i] = arr[k][j];
+                arr[k][j] = temp;
             }
             i++;
             j--;
