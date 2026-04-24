9260f616c3e1e9ea4901588fd9cf1c9e4d6a2f51
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2026-04-24 13:43:42 +0530

    Time: 1 ms (76.91%) | Memory: 46.7 MB (44.94%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,7 +1,5 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // toh unique value of index mei store hogi isliye no need to take 2 variables.
-
         int index = 1;
         for(int i=1;i<nums.length;i++){
             if(nums[i] != nums[i-1]){
