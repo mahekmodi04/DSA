7e9c304181d17e5155e9ded72176db519b493659
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2026-04-24 12:58:54 +0530

    Time: 2 ms (91.38%) | Memory: 47.5 MB (88.34%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,19 +1,11 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j=-1;
+        int j=0;//always for zero
         for(int i=0;i<nums.length;i++){
-            if(nums[i] == 0){
-                j=i;
-                break; //stores the first zero u get and break and start a new loop
-            }
-        }
-        if(j==-1)return;
-
-        for(int i=j+1;i<nums.length;i++){
             if(nums[i] != 0){
-                int t= nums[i];
-                nums[i] = nums[j];
-                nums[j] = t;
+                int t=nums[i];
+                nums[i] =nums[j];
+                nums[j] =t;
                 j++;
             }
         }
