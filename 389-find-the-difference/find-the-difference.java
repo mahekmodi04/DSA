ac68136424f1352c584186fdba94284f3e8e8311
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2026-04-27 12:43:28 +0530

    Time: 2 ms (59.86%) | Memory: 43.3 MB (38.48%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,22 +1,13 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        char[] c1 = s.toCharArray();
-        char[] c2 = t.toCharArray();
-
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-
-        int i=0;
-        int j=0;
-        while(i<c1.length && j<c2.length){
-            if(c1[i] != c2[j]){
-                return c2[j];
-            }
-            else{
-                i++;
-                j++;
-            }
+        //using XOR 
+        char xor = 0;
+        for(int i=0;i<s.length();i++){
+            xor ^= s.charAt(i);
         }
-        return c2[c2.length-1];
+        for(int j=0;j<t.length();j++){
+            xor ^= t.charAt(j);
+        }
+        return xor;
     }
 }
