a66ab568101df3bca834d7d04dca70375b14f984
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2026-05-01 18:20:32 +0530

    Time: 6 ms (84.60%) | Memory: 47.1 MB (38.75%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -4,14 +4,32 @@ class Solution {
         //No hashSet approach bcozz it gives unique from a string even if there are many. 
         //We want the char having only single freq so use hashmap.
 
-        HashMap<Character , Integer> map = new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) , 0) + 1);
+        // HashMap<Character , Integer> map = new HashMap<>();
+        // for(int i=0;i<s.length();i++){
+        //     map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) , 0) + 1);
+        // }
+
+        // for(int i=0;i<s.length();i++){
+        //     if(map.get(s.charAt(i)) == 1)return i;
+        // }
+        // return -1;
+
+
+
+        int[] freq = new int[26];
+
+        // count
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
         }
 
-        for(int i=0;i<s.length();i++){
-            if(map.get(s.charAt(i)) == 1)return i;
+        // find first unique
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
         }
+
         return -1;
     }
 }
