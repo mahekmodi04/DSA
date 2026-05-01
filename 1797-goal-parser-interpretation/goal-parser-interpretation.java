8864c8ec1824d41dca01233627c309e6d4fa8b81
mahekmodi04 <166978399+mahekmodi04@users.noreply.github.com>
2026-05-01 13:02:41 +0530

    Time: 1 ms (74.02%) | Memory: 43.2 MB (18.57%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -1,21 +1,23 @@
 class Solution {
     public String interpret(String command) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<command.length();i++){
-            if(command.charAt(i)=='G'){
-                sb.append('G');
-            }
-            else if(command.charAt(i) == '(' && command.charAt(i+1) == ')'){
+        // StringBuilder sb = new StringBuilder();
+        // for(int i=0;i<command.length();i++){
+        //     if(command.charAt(i)=='G'){
+        //         sb.append('G');
+        //     }
+        //     else if(command.charAt(i) == '(' && command.charAt(i+1) == ')'){
                 
-                sb.append('o');
-                i++;
-            }
-            else{
-                sb.append('a');
-                sb.append('l');
-                i+=3;
-            }
-        }
-        return sb.toString();
+        //         sb.append('o');
+        //         i++;
+        //     }
+        //     else{
+        //         sb.append('a');//cannot accept al together in append so do it alag
+        //         sb.append('l');
+        //         i+=3;
+        //     }
+        // }
+        // return sb.toString();
+
+        return command.replace("()", "o").replace("(al)", "al");
     }
 }
