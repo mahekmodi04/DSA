124761c917986b8bd4b9786d291e65d1d66f73d4
Mahek Modi <166978399+mahekmodi04@users.noreply.github.com>
2026-09-19 18:44:05 +0530

    Time: 6 ms (34.46%) | Memory: 47.1 MB (14.76%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,37 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        if(s.isEmpty())return true;
+        String s1 = s.toLowerCase();
+
         ArrayList<Character> list = new ArrayList<>();
-        s = s.toLowerCase();
-        int n = s.length();
-        for(int i=0;i<n;i++){
-            char ch = s.charAt(i);
-            if(Character.isLetterOrDigit(ch)){
-                list.add(ch);
+        for(int i=0;i<s1.length();i++){
+            char c = s1.charAt(i);
+            if(Character.isLetterOrDigit(c)){
+                list.add(c);
             }
         }
-        // int j = list.size()-1;
-        // while(i<j){
-        //     if(!list.get(i).equals(list.get(j))){//in string check for .equals for checking equal characters instead of list.get(i) != list.get(j) -> this will check for objects different references in memory
-        //         return false;
-        //     }
-        //     else{
-        //         i++;
-        //         j--;
-        //     }
-        // }
+       
+        int i =0;
+        int j=list.size()-1;
+        while(i<=j){
+            
+            if(list.get(i) != list.get(j))return false;
+            i++;
+            j--; 
 
-        
-        return palindrome(0,list);
+        }
+        return true;
     }
-    
-    
-    boolean palindrome(int i , ArrayList<Character> list){
-         
-            if(i >= list.size()/2)return true;
-
-            if(!list.get(i).equals(list.get(list.size()-i-1))){
-                return false;
-            }
-            return palindrome(i+1,list);
-        }
 }
